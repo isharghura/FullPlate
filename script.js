17be988561d27cf408fb65e1b9800854aa08900b
isharghura <ishar.ghuras@gmail.com>
2024-02-18 13:10:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var calories = document.getElementById("calories");
+// var numMeals = document.getElementById("numMeals").selectValue;
+var plan = document.getElementById("plan");
+var generateBtn = document.getElementById("generate");
+
+function generatePlan() {
+
+}

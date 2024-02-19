3a10e4f15fe09e6d0cdc22c64b5432589fd9bfad
isharghura <ishar.ghuras@gmail.com>
2024-02-18 22:07:08 -0500

    found calories for food

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var calories = document.getElementById("calories");
 var numMeals = document.getElementById("numMeals");
 var plan = document.getElementById("plan");
 var generateBtn = document.getElementById("generate");
-var avgCalMeal = calories.value / numMeals.value;
 
 async function generatePlan() {
     const config = await fetch("config.json").then(response => response.json());
@@ -10,15 +9,22 @@ async function generatePlan() {
     const response = await fetch(apiUrl);
     const data = await response.json();
 
-    const filteredFoods = data.foods.filter(food => {
-        const foodCalories = food.foodNutrients.find(nutrient => nutrient.nutrientId == 1008
-            && nutrient.nutrientNumber >= avgCalMeal - 50 && nutrient.nutrientNumber >= avgCalMeal + 50);
-        return foodCalories;
-    })
+    var avgCalMeal = calories.value / numMeals.value;
+    let allFood = [];
 
-    const randomFood = filteredFoods[Math.ceil(Math.random() * filteredFoods.length)];
+    var pages = 1;
 
-    console.log(filteredFoods);
+    while (pages <= data.totalPages) {
+        const filteredFoods = data.foods.filter(food => {
+            const foodCalories = food.foodNutrients.find(nutrient => nutrient.Energy >= avgCalMeal - 100 && nutrient.Energy <= avgCalMeal + 100);
+            return foodCalories;
+        })
+        pages++;
+        allFood = allFood.concat(filteredFoods);
+    }
+
+    const randomFood = allFood[Math.ceil(Math.random() * allFood.length)];
+    console.log(data.foods[11].foodNutrients.find(nutrient => nutrient.nutrientId === 1008)?.value);
 
     return randomFood;
 }
